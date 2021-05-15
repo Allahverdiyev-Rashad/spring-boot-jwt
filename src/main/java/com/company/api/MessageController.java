13bbaec24feb80605f7157eecabda384d11a7f58
Allahverdiyev-Rashad <developer.rashad.allahverdiyev@gmail.com>
2021-05-16 01:00:00 +0400

    Initial commit

---
+++ src/main/java/com/company/api/MessageController.java
@@ -0,0 +1,16 @@
+package com.company.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+
+    @GetMapping
+    public ResponseEntity<String > getMessage() {
+        return ResponseEntity.ok("Hello JWT");
+    }
+}
